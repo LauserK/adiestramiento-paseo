83c890bd2e03a51c474887fae0cd47ba639cde5a
Capital Humano 01 <capitalhumano01@grupopaseo.local>
2017-02-28 15:29:13 -0400

    UPDATE MAX AND MIN

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	filter_horizontal = ('conceptos_aprobados',)
--- usuarios/models.py
+++ usuarios/models.py
@@ -2,16 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.conf import settings
-"""
+from instructivos.models import Concepto, Examen
+from django.core.validators import MaxValueValidator, MinValueValidator
 class UserProfile(models.Model):  
-    usuario = models.OneToOneField(User)        
-    cursos = models.ManyToManyField(Curso, blank=True)
-    suscripciones = (
-    	('gratuita', 'Gratuita'),
-    	('mensual', 'Mensual'),
-    	('anual', 'Anual'),
-    )
-    suscripcion = models.CharField(max_length=10, choices=suscripciones, default='gratuita')
+    usuario             = models.OneToOneField(User)        
+    conceptos_aprobados = models.ManyToManyField(Concepto)
+
     class Meta:
     	verbose_name='Perfil de usuario'
     def __unicode__(self):
@@ -22,4 +18,14 @@ class UserProfile(models.Model):
         if created:
             UserProfile.objects.create(usuario=instance)
     post_save.connect(create_profile, sender=User)
-"""
+
+class ExamenAprobado(models.Model):
+    usuario = models.ForeignKey(User)
+    examen  = models.ForeignKey(Examen)
+    nota    = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+
+    class Meta:
+        verbose_name='Examenes aprobados'
+
+    def __unicode__(self):
+        return "%s - %s" % (self.usuario.username, self.examen.concepto.titulo)
