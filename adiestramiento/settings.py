dba907d9b0f864e640f5e44485a2d92ba66d83fb
Kildare Lauser <medicenkil@gmail.com>
2017-03-26 08:52:28 -0400

    SOME UPDS

--- adiestramiento/settings.py
+++ adiestramiento/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'adiestramiento.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adiestramiento',
+        'USER': 'adiestramiento',
+        "PASSWORD": "123",
+        "HOST": "10.10.2.200",
+        "POST": '3306',
     }
 }
 
