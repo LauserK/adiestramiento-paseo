574afb459f22599732a204b96aa40505eff9354b
root <root@canaima-prueba.(none)>
2017-03-28 15:37:30 -0430

    Deploy

--- adiestramiento/settings.py
+++ adiestramiento/settings.py
@@ -126,8 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
+if DEBUG == True:
+     STATIC_ROOT = '/root/adiestramiento/static/'
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
+
+if DEBUG == False:
+     MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
+else:
+     MEDIA_ROOT = '/root/adiestramiento/media/'
+
+
